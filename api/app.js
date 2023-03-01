4d7647d7c2cd502051e6ca2524cff59cd43ec74b
Electrolytful <erhanulusal@hotmail.com>
2023-03-01 17:33:22 +0000

    added logger middleware

--- api/app.js
+++ api/app.js
@@ -1,34 +1,29 @@
 // set up express, .env config to use secret variables
-require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
-const fruits = require('./fruits.json');
 const fs = require('fs');
-
-const port = process.env.PORT;
-const server = express();
-
-// make server listen on the specified port
-server.listen(port, () => {
-    console.log(`App is listening at port ${port}`);
-})
+const fruits = require('./fruits.json');
+const logger = require('./logger.js');
+const app = express();
 
 // adding the use of middleware
-server.use(express.json());
-server.use(cors());
+app.use(logger);
+app.use(cors());
+app.use(express.json());
+
 
 // Home route
-server.get('/', (req, res) => {
+app.get('/', async (req, res, next) => {
     res.status(200).send('Hello, Fruity API');
 })
 
 // Return all the fruits route
-server.get('/fruits', (req, res) => {
+app.get('/fruits', (req, res) => {
     res.status(200).send(fruits);
 })
 
 // Return a single fruit route
-server.get('/fruits/:name', (req, res) => {
+app.get('/fruits/:name', (req, res, next) => {
     let {name} = req.params;
     let object;
 
@@ -39,21 +34,21 @@ server.get('/fruits/:name', (req, res) => {
         }
     })
 
-    // if object is found and exists send it to client, else send 404 error
+    // if object is found and exists send it to client, else go next
     if(object) {
         res.status(200).send(object);
     } else {
-        res.status(404).send();
+        next();
     }
 })
 
 // adding fruit to the array and writing that change to the fruits.json file
-server.post('/fruits', (req, res) => {
+app.post('/fruits', (req, res) => {
     // check if fruit exists, if not add it using req.body, else return an error
     let object = req.body;
     let exists = false;
     let idExists = false;
-    object.id = Math.floor(Math.random() * 1000);
+    object.id = Math.floor(Math.random() * 20000);
 
     fruits.forEach(obj => {
         if(object.name.toLowerCase() === obj.name.toLowerCase()) {
@@ -66,14 +61,16 @@ server.post('/fruits', (req, res) => {
     })
 
     while(idExists) {
-        object.id = Math.floor(Math.random() * 1000);
+        object.id = Math.floor(Math.random() * 20000);
     }
 
     if(exists) {
-        res.status(409).send(`Error: ${object.name} already exists!`);
+        res.status(409).send();
     } else {
         fruits.push(object);
         fs.writeFileSync('./fruits.json', JSON.stringify(fruits));
-        res.status(201).send('New fruit added successfully!');
+        res.status(201).send(object);
     }
 })
+
+module.exports = app;
